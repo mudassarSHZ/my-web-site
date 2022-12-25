2aa4009b1f2d6b73a651b08f1feeb2c440532fdd
Mudassar Yasen <yasenmud@corp.contoursoftware.net>
2022-12-25 18:39:54 +0500

    Added check for string length in palindrome.js and modified HTML form element

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,22 @@
-function isPalindrome(string) {
-    // Remove all non-alphanumeric characters and convert the string to lowercase
-    let cleanString = string.replace(/[^a-z0-9]/gi, '').toLowerCase();
-  
-    // Check if the string is a palindrome by comparing it to its reverse
-    return cleanString === cleanString.split('').reverse().join('');
+// palindrome.js
+if (string.length > 10) {
+    console.log("The string is too long. Please enter a string with 10 characters or fewer.");
+    return;
   }
   
-  // Test the function with a few examples
-  console.log(isPalindrome('racecar')); // true
-  console.log(isPalindrome('hello')); // false
-  console.log(isPalindrome('A man, a plan, a canal: Panama')); // true
+function checkPalindrome(string) {
+    // Check if the string is greater than 10 characters
+    if (string.length > 10) {
+      return false;
+    }
+      
+    // Reverse the string and check if it is equal to the original
+    const reversedString = string.split('').reverse().join('');
+    return string === reversedString;
+  }
+  
+  // Test the function with different strings
+  console.log(checkPalindrome('racecar')); // true
+  console.log(checkPalindrome('hello')); // false
+  console.log(checkPalindrome('abcdefghijk')); // false
+  
